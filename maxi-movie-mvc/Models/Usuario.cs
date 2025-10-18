1d2a5babac0d2e7e4164cccac124c0c0d869ddd8
msarfernandez <msarfernandez@gmail.com>
2025-10-18 10:49:59 -0300

    modelo de clases inicial

---
+++ maxi-movie-mvc/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace maxi_movie_mvc.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string ImagenUrlPerfil { get; set; }
+
+        public List<Favorito> PeliculasFavoritas { get; set; }
+
+    }
+}
