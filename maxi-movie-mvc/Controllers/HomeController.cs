7c28692fb2f6d28a6dd2f451e6edad262148b5e8
msarfernandez <msarfernandez@gmail.com>
2025-10-18 14:41:29 -0300

    datatable js en lista de películas. Dark Mode. Home.

--- maxi-movie-mvc/Controllers/HomeController.cs
+++ maxi-movie-mvc/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
+using maxi_movie_mvc.Data;
 using maxi_movie_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace maxi_movie_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MovieDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MovieDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var peliculas = await _context.Peliculas.ToListAsync();
+            return View(peliculas);
         }
 
         public IActionResult Privacy()
