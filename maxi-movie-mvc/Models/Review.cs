5c687dd3ee7accd0f0f5a940019efaa31b9b5311
msarfernandez <msarfernandez@gmail.com>
2025-10-20 21:34:46 -0300

    Crear Review y validar que pueda crear solo una.

--- maxi-movie-mvc/Models/Review.cs
+++ maxi-movie-mvc/Models/Review.cs
@@ -21,4 +21,21 @@ namespace maxi_movie_mvc.Models
         [Timestamp]
         public byte[] RowVersion { get; set; }
     }
+
+    public class ReviewCreateViewModel
+    {
+        public int? Id { get; set; }
+        public int PeliculaId { get; set; }
+        public string? PeliculaTitulo { get; set; }
+        public string UsuarioId { get; set; } = string.Empty;
+
+        [Range(1, 5, ErrorMessage = "La calificación debe ser entre 1 y 5 estrellas")]
+        [Required(ErrorMessage = "La calificación es requerida")]
+        public int Rating { get; set; }
+
+        [StringLength(500, ErrorMessage = "El comentario no puede exceder 500 caracteres")]
+        [Required(ErrorMessage = "El comentario es requerido")]
+        public string Comentario { get; set; } = string.Empty;
+    }
+
 }
